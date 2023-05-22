31dbef3b501f429a562b6e4bef71a06483422025
pluswebsite <98026305+pluswebsite@users.noreply.github.com>
2023-05-22 17:39:29 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
 //functions
 var dobf = {};
     dobf.vari = {};
+    
+    //variable creation
     dobf.create = function(v, id="id", binid, key="key", mkey){
       dobf.vari[v] = {"id": binid, "key": mkey}
+    }
+
+    //DB acces
+    dobf.get = function(w, from, bin, to="to", call="console.log"){
+        let req = new XMLHttpRequest();
+
+req.onreadystatechange = () => {
+  if (req.readyState == XMLHttpRequest.DONE) {
+      if(w === '*'){
+        call(req.responseText);
+      }
+      else{
+        call(JSON.parse(req.responseText)[w])
+      }
+  }
+};
+
+req.open("GET", "https://api.jsonbin.io/v3/b/" + dobf.vari[bin].id + "/?meta=false", true);
+req.setRequestHeader("X-Master-Key", dobf.vari[bin].key);
+req.send();
     }
 
 class DOBS{
