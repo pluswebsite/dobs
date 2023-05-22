4e67354125619151926f9e9716d81d391565d505
pluswebsite <98026305+pluswebsite@users.noreply.github.com>
2023-05-22 17:30:03 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //functions
 var dobf = {};
     dobf.vari = {};
-    dobf.create = function(v, id, binid, key, mkey){
+    dobf.create = function(v, id="id", binid, key="key", mkey){
       dobf.vari[v] = {"id": binid, "key": mkey}
     }
 
@@ -10,6 +10,11 @@ class DOBS{
     let arr = q.split(" ")
     let fun = arr[0];
     arr.shift();
+    for(a in arr){
+        if(arr[a][0] != "'"){
+            arr[a] = "'" + arr[a] + "'";
+        }
+    }
     let str = arr.toString();
     eval("dobf." + fun + "(" + str + ")")
   }
