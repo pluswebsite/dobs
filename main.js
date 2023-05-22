6d5c320d911d5dc0f3428203fc79ed7ebd972197
pluswebsite <98026305+pluswebsite@users.noreply.github.com>
2023-05-22 16:00:47 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,9 +6,6 @@ class DOBS{
     let arr = q.split(" ")
     let fun = arr[0];
     arr.shift();
-    for(a in arr){
-      arr[a] = "'" + arr[a] + "'";
-    }
     let str = arr.toString();
     eval(fun + "(" + str + ")")
   }
