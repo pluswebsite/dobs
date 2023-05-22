1b0b411a3ad94d73d29664e6c0d8efaa4dfe6eff
pluswebsite <98026305+pluswebsite@users.noreply.github.com>
2023-05-22 16:14:36 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 class DOBS{
   constructor(){
-    
+    var dobf = {};
   }
   run(q){
     let arr = q.split(" ")
     let fun = arr[0];
     arr.shift();
     let str = arr.toString();
-    eval(fun + "(" + str + ")")
+    eval("dobf." + fun + "(" + str + ")")
   }
 }
