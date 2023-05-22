42208702154dafda4fdaaadfea48103bb278caed
pluswebsite <98026305+pluswebsite@users.noreply.github.com>
2023-05-22 17:26:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-class DOBS{
-  constructor(){
-    var dobf = {};
+//functions
+var dobf = {};
     dobf.vari = {};
     dobf.create = function(v, id, binid, key, mkey){
       dobf.vari[v] = {"id": binid, "key": mkey}
     }
-  }
+
+class DOBS{
   run(q){
     let arr = q.split(" ")
     let fun = arr[0];
