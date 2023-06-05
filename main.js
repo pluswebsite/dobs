03da1eb0aa70ba43cb80c869d609cf274e143906
pluswebsite <98026305+pluswebsite@users.noreply.github.com>
2023-06-05 14:50:08 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,8 +5,9 @@ function _(script){
         arr.shift();
         for(let a in arr){
             if(arr[a][0] == "%"){
-                arr[a] = "'" + eval(arr[a].replaceAll("%","")) + "'";
-            } else if(arr[a][0] != "'"){
+                arr[a] = eval(arr[a].replaceAll("%",""));
+            }
+            if(arr[a][0] != "'"){
                 arr[a] = "'" + arr[a] + "'";
             }
         }
